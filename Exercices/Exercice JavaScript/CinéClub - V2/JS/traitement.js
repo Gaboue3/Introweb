5b209f3017693887f06019c6ec2944ae9f39b7a6
Gaaaaaaaaaben <gabriel-ouellet-1999@hotmail.com>
2018-10-26 10:54:31 -0400

    Fini cineclubvv2

---
+++ Exercices/Exercice JavaScript/CinéClub - V2/JS/traitement.js
@@ -0,0 +1,49 @@
+function btnCalculer_onclick()
+{
+var prixBase,ageSaisi,coutFinale, film,temps;
+
+prixBase=parseFloat(document.getElementById("txtBasePrix").value);
+ageSaisi=parseInt(document.getElementById("txtAge").value);
+film=document.getElementById("lstOptions").value;
+if(document.getElementById("radMidi").checked==true)
+{
+    temps="midi";
+}
+else
+{
+    temps="soir";
+}
+
+
+if(document.getElementById("chkMiSession").checked==true)
+{
+            if (ageSaisi<=16)
+        {
+                if(document.getElementById("radMidi").checked==true)
+                {
+                    coutFinale=prixBase-(prixBase*60/100);
+                }
+                else
+                {
+                    coutFinale=prixBase-(prixBase*40/100);
+                }
+        }
+        else
+        {
+                if(document.getElementById("radMidi").checked==true)
+                {
+                    coutFinale=prixBase-(prixBase*30/100);
+                }
+                else
+                {
+                    coutFinale=prixBase;
+                }
+        }
+}
+else
+{
+    coutFinale=prixBase;
+}
+
+console.log("Un billet du "+temps+" pour un spectateur de "+ageSaisi+" ans coute $"+ coutFinale+" pour le film "+film);
+}
